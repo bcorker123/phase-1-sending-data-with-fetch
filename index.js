9a3c5d69440e2af03ec4dec45a19ddb5f9ba82e8
brian corker <bcorker123@gmail.com>
2022-05-03 16:41:05 -0400

    commit tree e9049183fb90d9265f968d5fb2ba9ffc8c6e6576

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+function submitData(userName, userEmail){
+    //defining fetch request object using submitData's parameters
+    const configObject = {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json",
+            "Accept":"application/json"
+        },
+        body:JSON.stringify({
+            name:userName,
+            email:userEmail
+        })
+    }
+    
+
+    
+    return fetch('http://localhost:3000/users', configObject)
+    .then(resp => resp.json())
+    .then(object => {
+        const h1 = document.createElement('h1');
+        h1.textContent = object.id
+        document.body.appendChild(h1)
+    })
+    .catch(error => {
+        const h2 = document.createElement('h2')
+        h2.textContent = error.message
+        document.body.appendChild(h2)
+    })
+}
